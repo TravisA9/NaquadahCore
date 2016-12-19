751613c5b607c3ef505db7085bd0eb54489c1035
TravisA9 <travisashworth2007@gmail.com>
2016-12-19 16:56:17 -0600

    Just playing

--- src/Drawing/GUI.jl
+++ src/Drawing/GUI.jl
@@ -1,7 +1,7 @@
 # http://giovineitalia.github.io/Compose.jl/latest/#Trees-can-be-visualized-with-introspect-1
 using Compose, Gtk, Colors, Measures
 
-c = Gtk.@GtkCanvas(400,300);
+c = Gtk.@GtkCanvas(800,600);
 w = Gtk.@GtkWindow(c,"Default GUI");
 show(c);
 # ==============================================================================
@@ -22,37 +22,47 @@ end
 #         (context(), circle(), fill("bisque")),
 #         (context(), rectangle(), fill("tomato")))
 #draw(SVG("tomato_bisque.svg", 4cm, 4cm), composition)
-set_default_graphic_size(6cm, 4cm) # hide
 
 figsize = 6mm
-#=
-t = table(3, 2, 1:3, 2:2, y_prop=[1.0, 1.0, 1.0])
-t[1,1] = [compose(context(minwidth=figsize + 2mm, minheight=figsize),
-                  circle(0.5, 0.5, figsize/2), fill(LCHab(92, 10, 77)))]
-t[2,1] = [compose(context(minwidth=figsize + 2mm, minheight=figsize),
-                  rectangle(0.5cx - figsize/2, 0.5cy - figsize/2, figsize, figsize),
-                  fill(LCHab(68, 74, 192)))]
-t[3,1] = [compose(context(minwidth=figsize + 2mm, minheight=figsize),
-                  polygon([(0.5cx - figsize/2, 0.5cy - figsize/2),
-                           (0.5cx + figsize/2, 0.5cy - figsize/2),
-                           (0.5, 0.5cy + figsize/2)]),
-                  fill(LCHab(68, 74, 29)))]
-t[1,2] = [compose(context(), text(0, 0.5, "Context", hleft, vcenter))]
-t[2,2] = [compose(context(), text(0, 0.5, "Form", hleft, vcenter))]
-t[3,2] = [compose(context(), text(0, 0.5, "Property", hleft, vcenter))]
-composition = compose(context(), t, fill(LCHab(92, 10, 77)), fontsize(10pt))=#
-composition = compose(context(),
-            (context(), circle(), fill("bisque")),
-            (context(), rectangle(), fill("tomato")))
-comp = introspect(composition)
+
+function Element(top,left,height,width)
+    txt = compose(context(),
+    text(top,left, "This is some test text to test my text box.", hleft,vtop), fill(LCHab(10, 10, 77)), fontsize(15px))
+    t = compose(context(minwidth=figsize + 2px, minheight=figsize),
+                      rectangle(top,left,width,height), fill(LCHab(68, 74, 192)),
+                      txt)
+# stroke(c::(@compat Void))
+    #=t = table(3, 2, 1:3, 2:2, y_prop=[1.0, 1.0, 1.0])
+    t[1,1] = [compose(context(minwidth=figsize + 2mm, minheight=figsize),
+                      rectangle(top,left,width,height), fill(LCHab(68, 74, 192)))]
+    t[2,1] = [compose(context(minwidth=figsize + 2mm, minheight=figsize),
+                      circle(0.5, 0.5, figsize/2), fill(LCHab(92, 10, 77)))]
+    t[3,1] = [compose(context(minwidth=figsize + 2mm, minheight=figsize),
+                      polygon([(0.5cx - figsize/2, 0.5cy - figsize/2),
+                               (0.5cx + figsize/2, 0.5cy - figsize/2),
+                               (0.5, 0.5cy + figsize/2)]),
+                      fill(LCHab(68, 74, 29)))]
+    t[1,2] = [compose(context(), text(top,left, "This is some test text to test my text box.", hleft, vcenter))]
+    t[2,2] = [compose(context(), text(0, 0.5, "Form", hleft, vcenter))]
+    t[3,2] = [compose(context(), text(0, 0.5, "Property", hleft, vcenter))]=#
+    #composition =
+    return compose(context(), t)
+end
+
+ composition = Element(100px,100px,200px,250px)
+
+
+
+
+       Gtk.draw(c)
+println(".......", composition )
 # ==============================================================================
 Gtk.draw(c) do widget
-       Compose.draw(CAIROSURFACE(c.back),comp)
+       Compose.draw(CAIROSURFACE(c.back),composition)
 end
 # ==============================================================================
-
-       #co = sierpinski(7);
-       Gtk.draw(c)
+# comp = compose(context(), circle(0.5, 0.5, figsize/2), fill("bisque"))
+# Gtk.draw(c)
 
 
 
